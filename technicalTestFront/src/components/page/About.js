d0630fedcb8cce7fdb7d9261b3d16f8baee755be
mednaderghorghar <mednaderghorghar@gmail.com>
2024-05-06 14:28:40 +0100

    adding about page

---
+++ technicalTestFront/src/components/page/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="text-center">
+      <h1>About</h1>
+      <p>
+        A To Do list application built in favor of onHands Technical Assessement
+      </p>
+    </div>
+  );
+}
